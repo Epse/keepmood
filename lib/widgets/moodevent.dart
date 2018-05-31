1b865f8fa5237cbcd6b364451f00878e10743d9c
Stef Pletinck <s@stefpletinck.be>
2018-05-31 19:09:56 +0200

    nicer time difference

--- lib/widgets/moodevent.dart
+++ lib/widgets/moodevent.dart
@@ -11,6 +11,28 @@ class MoodEvent extends StatelessWidget {
 
   bool notNull(Object o) => o != null;
 
+  String timeDifferenceString(DateTime time) {
+    int days = new DateTime.now().difference(time).inDays;
+    if (days == 0) {
+      int hours = new DateTime.now().difference(time).inHours;
+      if (hours == 1) {
+        return hours.toString() + " hour ago";
+      }
+      if (hours > 1) {
+        return hours.toString() + " hours ago";
+      }
+      int minutes = new DateTime.now().difference(time).inMinutes;
+      if (minutes == 1) {
+        return minutes.toString() + " minute ago";
+      }
+      return minutes.toString() + " minutes ago";
+    }
+    if (days == 1) {
+      return days.toString() + " day ago";
+    }
+    return days.toString() + " days ago";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -18,9 +40,7 @@ class MoodEvent extends StatelessWidget {
             children: <Widget>[
       ListTile(
           title: Text(sentimentString(recording.sentiment)),
-          subtitle: Text(
-              recording.time.difference(new DateTime.now()).inDays.toString() +
-                  " Days ago")),
+          subtitle: Text(timeDifferenceString(recording.time))),
       (recording.comment != "") ? new Text(recording.comment) : null,
       new ButtonTheme.bar(
           child: new ButtonBar(
